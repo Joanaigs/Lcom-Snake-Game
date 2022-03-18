fc006920cbfc48b95a99ec3920e34eb289a87933
Joana Santos <up202006279@edu.fe.up.pt>
2022-03-18 15:58:27 +0000

    added classes lab2

--- lab2/lab2.c
+++ lab2/lab2.c
@@ -30,10 +30,9 @@ int main(int argc, char *argv[]) {
 }
 
 int(timer_test_read_config)(uint8_t timer, enum timer_status_field field) {
-  /* To be implemented by the students */
-  printf("%s is not yet implemented!\n", __func__);
-
-  return 1;
+  uint8_t state = 0;
+  if(timer_get_conf(timer, &state)) return 1;
+  if(timer_display_conf(timer, state, field)) return 1;
 }
 
 int(timer_test_time_base)(uint8_t timer, uint32_t freq) {
